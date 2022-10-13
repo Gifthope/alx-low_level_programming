456555d309e0e72a31670862b2158ca5d8685d74
Achobah Hope <eleojohope6@gmail.com>
2022-10-13 16:00:57 -0700

    my put commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 /**
- * Main - Entry
- * Return: Always 0
+ * main - Entry
+ * Return: Always 0 
  */
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-
 }
